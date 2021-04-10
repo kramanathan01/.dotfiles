613f6e29849cd56deeee46b4e585f3515553265e
kramanathan <5899848+kramanathan01@users.noreply.github.com>
2021-04-10 15:07:26 -0700

    removing architecture specific path
    Signed-off-by: kramanathan <5899848+kramanathan01@users.noreply.github.com>

--- antibody/install.sh
+++ antibody/install.sh
@@ -4,5 +4,5 @@ if which brew >/dev/null 2>&1; then
 else
 	curl -sL https://git.io/antibody | sh -s
 fi
-/opt/homebrew/bin/antibody bundle <"$DOTFILES/antibody/bundles.txt" >~/.zsh_plugins.sh
-/opt/homebrew/bin/antibody update
+antibody bundle <"$DOTFILES/antibody/bundles.txt" >~/.zsh_plugins.sh
+antibody update
