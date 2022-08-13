2a7406ae5f990ff73f9428a725e0d15b6fd5a8f6
kramanathan01 <5899848+kramanathan01@users.noreply.github.com>
2022-08-13 13:04:37 -0700

    background execution for Jagannatha Hora
    Signed-off-by: kramanathan01 <5899848+kramanathan01@users.noreply.github.com>

--- macos/aliases.zsh
+++ macos/aliases.zsh
@@ -1,3 +1,5 @@
 #!/bin/sh
 alias airport="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
 alias afk="open -a /System/Library/CoreServices/ScreenSaverEngine.app"
+alias jd="cd ~/.wine/drive_c/Program\ Files\ \(x86\)/Jagannatha\ Hora/data"
+alias jh="wine64 ~/.wine/drive_c/Program\ Files\ \(x86\)/Jagannatha\ Hora/bin/jhora.exe&"
